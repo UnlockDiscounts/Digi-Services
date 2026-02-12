ca074f5e1d6d4c0ddc012d5793de9023aaf6e32f
yagyasoni <yagyasony@gmail.com>
2026-02-12 05:36:05 +0530

    code update

--- vite.config.js
+++ vite.config.js
@@ -14,39 +14,39 @@ export default defineConfig({
     tailwindcss(),
     svgr(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "react-vendor": ["react", "react-dom", "react-router-dom"],
-          "ui-components": [
-            "./src/components/NavBar.jsx",
-            "./src/components/Footer.jsx",
-            "./src/components/sidebar/sidebar.jsx",
-          ],
-          landing: [
-            "./src/components/LandingPage/LandingPage.tsx",
-            "./src/components/LandingPage/HeroSection.jsx",
-            "./src/components/LandingPage/AboutSection.jsx",
-            "./src/components/LandingPage/ServicesSection.jsx",
-            "./src/components/LandingPage/PricingSection.jsx",
-            "./src/components/LandingPage/TestimonialsSection.jsx",
-            "./src/components/LandingPage/ArticlesSection.jsx",
-            "./src/components/LandingPage/ComparisonSection.jsx",
-          ],
-          services: [
-            "./src/pages/ServiceCategory.jsx",
-            "./src/pages/ServiceDetail.jsx",
-            "./src/components/services/service.jsx",
-          ],
-          blogs: [
-            "./src/pages/Articles.jsx",
-            "./src/pages/Blog.jsx",
-            "./src/components/blogs/blogs.jsx",
-          ],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         "react-vendor": ["react", "react-dom", "react-router-dom"],
+  //         "ui-components": [
+  //           "./src/components/NavBar.jsx",
+  //           "./src/components/Footer.jsx",
+  //           "./src/components/sidebar/sidebar.jsx",
+  //         ],
+  //         landing: [
+  //           "./src/components/LandingPage/LandingPage.tsx",
+  //           "./src/components/LandingPage/HeroSection.jsx",
+  //           "./src/components/LandingPage/AboutSection.jsx",
+  //           "./src/components/LandingPage/ServicesSection.jsx",
+  //           "./src/components/LandingPage/PricingSection.jsx",
+  //           "./src/components/LandingPage/TestimonialsSection.jsx",
+  //           "./src/components/LandingPage/ArticlesSection.jsx",
+  //           "./src/components/LandingPage/ComparisonSection.jsx",
+  //         ],
+  //         services: [
+  //           "./src/pages/ServiceCategory.jsx",
+  //           "./src/pages/ServiceDetail.jsx",
+  //           "./src/components/services/service.jsx",
+  //         ],
+  //         blogs: [
+  //           "./src/pages/Articles.jsx",
+  //           "./src/pages/Blog.jsx",
+  //           "./src/components/blogs/blogs.jsx",
+  //         ],
+  //       },
+  //     },
+  //   },
+  //   chunkSizeWarningLimit: 1000,
+  // },
 });
